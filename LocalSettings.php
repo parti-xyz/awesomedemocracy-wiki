5e816056a968b2466c54b348e9046e4e129fd062
dali <dalikim@parti.xyz>
2018-01-22 14:13:31 +0900

    bureaucrat는 사용자 권한 설정할 수 있는 권한이 생깁니다

--- LocalSettings.php
+++ LocalSettings.php
@@ -119,6 +119,7 @@ $wgDiff3 = "/usr/bin/diff3";
 
 # The following permissions were set based on your choice in the installer
 $wgGroupPermissions['*']['edit'] = false;
+$wgGroupPermissions['bureaucrat']['userrights'] = true;
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, ie 'vector', 'monobook':
